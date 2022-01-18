a59756726baddbf3a1bb658696920833750cc46c
LazarevaKate <lazareva.ekaterina2yandex.ru>
2022-01-18 23:32:00 +0300

    Все задание сделано, pytest пройден

---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+POSTS_COUNT = 10
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:POSTS_COUNT]
+    context = {
+        'text': "Это главная страница проекта Yatube",
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    "Здесь будет информация о группах проекта Yatube"
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POSTS_COUNT]
+    context = {
+        'text': "Здесь будет информация о группах проекта Yatube",
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, "posts/group_list.html", context)
+
+
+# Create your views here.
